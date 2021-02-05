52f003fc6b575eb9bb9c8bc2832a3b7cba51fd67
Vitor Zanoni <vitorzanonib@gmail.com>
2021-02-05 20:48:50 -0300

    refatoracao

---
+++ src/superPatinhas/projetosuperpatinhas/AnimalAdocao.java
@@ -0,0 +1,42 @@
+
+package superPatinhas.projetosuperpatinhas;
+
+/**
+ *
+ * @author carol
+ */
+public class AnimalAdocao{
+    private String linkRedeSocial;
+    private String adotadoAnteriormente;
+    private String ongPerternce;
+
+    public String getLinkRedeSocial() {
+        return linkRedeSocial;
+    }
+
+    public void setLinkRedeSocial(String linkRedeSocial) {
+        this.linkRedeSocial = linkRedeSocial;
+    }
+
+    public String getAdotadoAnteriormente() {
+        return adotadoAnteriormente;
+    }
+
+    public void setAdotadoAnteriormente(String adotadoAnteriormente) {
+        this.adotadoAnteriormente = adotadoAnteriormente;
+    }
+
+    public String getOngPerternce() {
+        return ongPerternce;
+    }
+
+    public void setOngPerternce(String ongPerternce) {
+        this.ongPerternce = ongPerternce;
+    }
+    
+    public AnimalAdocao(String linkRedeSocial, String adotadoAnteriormente, String ongPerternce) {
+        this.linkRedeSocial = linkRedeSocial;
+        this.adotadoAnteriormente = adotadoAnteriormente;
+        this.ongPerternce = ongPerternce;
+    }
+}
---
+++ src/superPatinhas/projetosuperpatinhas/AnimalPerdido.java
@@ -0,0 +1,26 @@
+
+package superPatinhas.projetosuperpatinhas;
+
+/**
+ *
+ * @author carol
+ */
+public class AnimalPerdido{
+    
+    private String localVisto;
+
+    public String getLocalVisto() {
+        return localVisto;
+    }
+
+    public void setLocalVisto(String localVisto) {
+        this.localVisto = localVisto;
+    }
+
+    public AnimalPerdido(String localVisto) {
+        this.localVisto = localVisto;
+    }
+
+   
+    }
+   
